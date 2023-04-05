925f139e29e560e9fd1beb598ce0a0ae41342131
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-04-05 16:01:57 +0200

    Add project files.

---
+++ csharp-typesystem-snacks/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_typesystem_snacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Piccoli Snack!");
+
+
+            //Snack 1
+
+            //int first = Convert.ToInt32(Console.ReadLine());
+            //int second = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine($"Tra {first} & {second} è maggiore {GetMax(first, second)}");
+
+
+
+            //Snack 2
+
+            //String firstWord = Console.ReadLine();
+            //String secondWord = Console.ReadLine();
+
+
+
+            //if( firstWord.Length == secondWord.Length )
+            //{
+            //    Console.WriteLine($"Hanno lunghezzauguale {firstWord} {secondWord}");
+            //}
+            //else if(firstWord.Length > secondWord.Length) {
+
+            //    Console.WriteLine($"Tra {firstWord} & {secondWord} è maggiore {firstWord}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Tra {firstWord} & {secondWord} è maggiore {secondWord}");
+            //}
+
+
+
+            //Snack 3
+            //int sum = 0;
+            //for(int i=0; i<10; i++)
+            //{
+            //    int number = Convert.ToInt32(Console.ReadLine());
+            //    sum += number;
+            //}
+            //    Console.WriteLine($"{sum}");
+
+
+
+
+            //Snack 4
+
+            //int sum = 0;
+
+            //for(int i = 2; i <= 10; i++)
+            //{
+            //    sum += i;
+            //}
+            //double average = (double)sum / 9;
+            //Console.WriteLine("La somma dei numeri da 2 a 10 è: " + sum);
+            //Console.WriteLine("La media dei numeri da 2 a 10 è: " + average);
+
+
+
+            //Snack 5
+
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            ////Dispari
+            //if (number % 2 != 0)
+            //{
+            //    Console.WriteLine($"Numero: {number + 1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Numero: {number}");
+            //}
+
+
+
+
+            //Snack 6
+            //String[] partyList = { "Vincenzo", "Gloria", "Giovanni", "Giacomo", "Davide", "Elena" };
+
+            //Console.WriteLine("Name: ");
+
+            //String name = Console.ReadLine();
+            //bool invitato = false;
+
+            //foreach (String party in partyList)
+            //{
+            //    if (party.ToLower() == name.ToLower())
+            //    {
+            //        invitato = true;
+            //        break;
+            //    }
+            //}
+
+            //if (invitato)
+            //    Console.WriteLine("sei dentro");
+            //else 
+            //    Console.WriteLine("Non sei invitato");
+
+        }
+
+        //Snack 1
+        //public static int GetMax(int first, int second)
+        //{
+        //    return first > second ? first : second;
+        //}
+
+
+
+        
+
+    }
+}
